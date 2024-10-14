255f1621595af3925e2c0337dd497b646312e6fc
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-10-14 14:09:43 +0100

    task 4, GET articleByID

--- controllers/getControllers.js
+++ controllers/getControllers.js
@@ -1,9 +1,20 @@
-const { fetchTopics } = require("../models/fetchModels");
+const { fetchTopics, fetchArticleById } = require("../models/fetchModels");
 
 exports.getTopics = (req, res, next) => {
   fetchTopics()
     .then((topics) => {
       res.status(200).send({ topics: topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticleByID = (req, res, next) => {
+  const { article_id } = req.params;
+  fetchArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article: article });
     })
     .catch((err) => {
       next(err);
--- models/fetchModels.js
+++ models/fetchModels.js
@@ -8,3 +8,18 @@ exports.fetchTopics = () => {
     return topics.rows;
   });
 };
+
+exports.fetchArticleById = (articleID) => {
+  return db
+    .query(
+      `SELECT * FROM articles
+        WHERE article_id = $1`,
+      [articleID]
+    )
+    .then((article) => {
+      if (article.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Entry not found" });
+      }
+      return article.rows[0];
+    });
+};

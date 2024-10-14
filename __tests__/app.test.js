91d67d2b1a927437758aac89fcd700a1d5160020
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-10-14 12:17:21 +0100

    task 3, general api endpoint added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const data = require("../db/data/test-data/index.js");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed.js");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -30,6 +31,17 @@ describe("200: GET /api/topics", () => {
           expect(typeof topic.description).toBe("string");
           expect(typeof topic.slug).toBe("string");
         });
+      });
+  });
+});
+
+describe("200 GET /api", () => {
+  test("should respond with a 200 status and a JSON object that lists and details all current functional endpoints on my API.", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.endpoints).toEqual(endpoints);
       });
   });
 });

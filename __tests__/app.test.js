07b6fb7ce8e24e76eff5f05d4643d1f1d94ca788
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-10-15 16:01:07 +0100

    added first delete method for deleting specific comments by their ID

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -266,3 +266,31 @@ describe("PATCH /api/articles/:article_id", () => {
       });
   });
 });
+
+describe("DELETE /api/comments/:comment_id", () => {
+  test("204: Should remove a comment from our database by providing the comment_id of the comment we intend to remove.", () => {
+    return request(app)
+      .delete("/api/comments/1")
+      .expect(204)
+      .then((response) => {
+        expect(response.statusCode).toBe(204);
+      });
+  });
+  test("404: Should return an error when attempting to delete a comment with a valid comment_id type, but doesnt exist", () => {
+    return request(app)
+      .delete("/api/comments/99999")
+      .expect(404)
+      .then((response) => {
+        expect(response.statusCode).toBe(404);
+      });
+  });
+  test("400: Should return an error when attempting to delete a comment with a invalid comment_id type", () => {
+    return request(app)
+      .delete("/api/comments/hello")
+      .expect(400)
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body.msg).toBe("Bad request");
+      });
+  });
+});

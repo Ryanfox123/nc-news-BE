e1e39c04a043aeef98ea9d68aae41f961c9678e6
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-10-16 15:12:27 +0100

    get article by ID should now also respond with comment_count on its body.

--- models/fetchModels.js
+++ models/fetchModels.js
@@ -12,8 +12,15 @@ exports.fetchTopics = () => {
 exports.fetchArticleById = (articleID) => {
   return db
     .query(
-      `SELECT * FROM articles
-        WHERE article_id = $1`,
+      `SELECT articles.article_id, articles.votes, articles.title, 
+      articles.topic, articles.author, articles.body, 
+      articles.created_at, articles.article_img_url,
+      COUNT(comments.comment_id) AS comment_count
+      FROM articles
+      LEFT JOIN comments
+      ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1 
+      GROUP BY articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.article_img_url, articles.votes;`,
       [articleID]
     )
     .then((article) => {

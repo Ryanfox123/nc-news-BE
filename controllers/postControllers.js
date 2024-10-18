40e1dd09f0414e711e69aa8ee8607ceb8ac4e0e9
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-10-18 12:27:02 +0100

    added new post article method

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,5 +1,5 @@
-const { createComment } = require("../models/createModels");
-const { fetchArticleById } = require("../models/fetchModels");
+const { createComment, createArticle } = require("../models/createModels");
+const { fetchArticleById, fetchTopics } = require("../models/fetchModels");
 
 exports.postComment = (req, res, next) => {
   const { article_id } = req.params;
@@ -16,6 +16,30 @@ exports.postComment = (req, res, next) => {
       res.status(200).send({ comment: comment });
     })
     .catch((err) => {
+      next(err);
+    });
+};
+
+exports.postArticle = (req, res, next) => {
+  const body = req.body;
+  createArticle(body)
+    .then((article) => {
+      res.status(200).send({ article: article });
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.constraint === "articles_topic_fkey") {
+        err = {
+          status: 404,
+          msg: "Article does not exist: please post to an existant article",
+        };
+      }
+      if (err.constraint === "articles_author_fkey") {
+        err = {
+          status: 404,
+          msg: "This user does not exist: please post from an existing user",
+        };
+      }
       next(err);
     });
 };
